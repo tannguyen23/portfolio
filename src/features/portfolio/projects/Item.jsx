251017bccce5dc6ca6d5c22f52b250afbf364724
T-Code <tanta2358@gmail.com>
2025-01-17 12:00:55 +0700

    Make 3d carousel

--- src/features/portfolio/projects/Item.jsx
+++ src/features/portfolio/projects/Item.jsx
@@ -1,18 +1,20 @@
-export default function Item() {
+export default function Item({ style }) {
     return (
-        <div className="w-[36rem] h-[24rem] relative">
-            <div
-                className="w-[36rem] h-[24rem] bg-[#6c63ff] bg-opacity-15 border-[#6c63ff] border-opacity-50 border-1
-                          backdrop-blur-md shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#6c63ff,0_0_15px_#6c63ff,0_0_30px_#6c63ff]
-                          rounded-2xl absolute top-0 "
-            ></div>
-            <div
-                className="w-[36rem] h-[60px] rounded-t-2xl absolute -bottom-[62px]
-                bg-[#6c63ff] bg-opacity-15 border-[#6c63ff] border-opacity-50 border-1
-                backdrop-blur-md shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#6c63ff,0_0_15px_#6c63ff,0_0_30px_#6c63ff]
-                bg-gradient-to-b from-[#6c63ff] to-transparent
-            "
-            ></div>
-        </div>
+            <div className="w-[36rem] h-[24rem] relative" style={style}>
+                {/* Card with Neon Effect */}
+                <div
+                    className="w-[36rem] h-[24rem] bg-[#6c63ff] bg-opacity-15 border-[#6c63ff] border-opacity-50 border-1
+                  backdrop-blur-md shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#6c63ff,0_0_15px_#6c63ff,0_0_30px_#6c63ff]
+                  rounded-2xl absolute top-0"
+                ></div>
+
+                {/* Reflection with more height and adjusted position */}
+                <div
+                    className="w-[36rem] h-[150px] rounded-t-2xl absolute -bottom-[156px] bg-gradient-to-b from-[#6c63ff] to-transparent
+                opacity-30 backdrop-blur-md
+                shadow-[0_0_2px rgba(255,255,255,1), 0_0_10px rgba(108,99,255,0.5)] 
+                transform scale-y-100"
+                ></div>
+            </div>
     );
 }
--- src/features/portfolio/projects/index.jsx
+++ src/features/portfolio/projects/index.jsx
@@ -1,17 +1,91 @@
+import { useState } from "react";
 import GlowingText from "../../../components/GlowingText";
 import Item from "./Item";
 
+export const PROJECTS = [
+    {
+        id: 1,
+        name: "A",
+    },
+    {
+        id: 2,
+        name: "B",
+    },
+    {
+        id: 3,
+        name: "C",
+    },
+    // {
+    //     id: 4,
+    //     name: "D",
+    // },
+];
+
 export default function Projects() {
+    const [activeSlide, setActiveSlide] = useState(1);
+
+    const getStyles = (index) => {
+        if (activeSlide === index)
+            return {
+                opacity: 1,
+                transform: "translateX(0px) translateZ(0px) rotateY(0deg)",
+                zIndex: 10,
+            };
+        else if (activeSlide - 1 === index)
+            return {
+                opacity: 1,
+                transform:
+                    "translateX(100px) translateZ(-100px) rotateY(25deg)",
+                zIndex: 9,
+            };
+        else if (activeSlide + 1 === index)
+            return {
+                opacity: 1,
+                transform:
+                    "translateX(-100px) translateZ(-100px) rotateY(-25deg)",
+                zIndex: 9,
+            };
+        else if (activeSlide - 2 === index)
+            return {
+                opacity: 1,
+                transform:
+                    "translateX(-480px) translateZ(-500px) rotateY(35deg)",
+                zIndex: 8,
+            };
+        else if (activeSlide + 2 === index)
+            return {
+                opacity: 1,
+                transform:
+                    "translateX(480px) translateZ(-500px) rotateY(-35deg)",
+                zIndex: 8,
+            };
+        else if (index < activeSlide - 2)
+            return {
+                opacity: 0,
+                transform:
+                    "translateX(-480px) translateZ(-500px) rotateY(35deg)",
+                zIndex: 7,
+            };
+        else if (index > activeSlide + 2)
+            return {
+                opacity: 0,
+                transform:
+                    "translateX(480px) translateZ(-500px) rotateY(-35deg)",
+                zIndex: 7,
+            };
+    };
+
     return (
         <div className="flex flex-col h-full justify-start items-end md:mx-48">
             <GlowingText
                 text="Joined projects"
                 gradientColors="from-[#857eff] via-[#a69bff] to-[#c3baff]"
             />
-            <div className="flex flex-row w-full">
-              <Item/>
+            <div className="flex flex-row w-full" style={{perspective : "1000px"}}>
+                {PROJECTS.map((item, idx) => {
+                    return <Item key={idx} style={getStyles(idx)} />;
+                })}
             </div>
-
         </div>
     );
 }
