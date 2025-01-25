d5f3af03b91dadff081d2c0fc26805d38e8f941a
T-Code <tanta2358@gmail.com>
2025-01-25 10:52:30 +0700

    Handle small screen for my-stack ui

---
+++ src/features/portfolio/my-stack/constant.js
@@ -0,0 +1,69 @@
+export const STACKS_FE = [
+  {
+      name: "ReactJS",
+      src: "/reactjs-svgrepo-com.svg",
+  },
+  {
+      name: "Javascript",
+      src: "/javascript-svgrepo-com.svg",
+  },
+  {
+      name: "Material UI",
+      src: "/material-ui-svgrepo-com.svg",
+  },
+  {
+      name: "TailwindCSS",
+      src: "/tailwindcss-icon-svgrepo-com.svg",
+  },
+  {
+      name: "Redux",
+      src: "/redux-svgrepo-com.svg",
+  },
+  {
+      name: "HTML5",
+      src: "/html-5-svgrepo-com.svg",
+  },
+  {
+      name: "CSS3",
+      src: "/css-3-svgrepo-com.svg",
+  },
+  {
+      name: "Typescript",
+      src: "/typescript-svgrepo-com.svg",
+  },
+];
+
+export const STACKS_BE = [
+  {
+      name: "PHP",
+      src: "/php-svgrepo-com.svg",
+  },
+  {
+      name: "Laravel",
+      src: "/laravel-svgrepo-com.svg",
+  },
+  {
+      name: "NodeJS",
+      src: "/nodejs-svgrepo-com.svg",
+  },
+  {
+      name: "NestJS",
+      src: "/nestjs-svgrepo-com.svg",
+  },
+  {
+      name: "Docker",
+      src: "/docker-svgrepo-com.svg",
+  },
+  {
+      name: "Ubuntu",
+      src: "/ubuntu-svgrepo-com.svg",
+  },
+  {
+      name: "Nginx",
+      src: "/nginx-svgrepo-com.svg",
+  },
+  {
+      name: "PostgreSQL",
+      src: "/postgresql-svgrepo-com.svg",
+  },
+];
--- src/features/portfolio/my-stack/index.jsx
+++ src/features/portfolio/my-stack/index.jsx
@@ -1,150 +1,110 @@
+import Button from "../../../components/Button";
 import GlowingText from "../../../components/GlowingText";
+import { STACKS_BE, STACKS_FE } from "./constant";
 
-const STACKS_FE = [
-    {
-        name: "ReactJS",
-        src: "/reactjs-svgrepo-com.svg",
-    },
-    {
-        name: "Javascript",
-        src: "/javascript-svgrepo-com.svg",
-    },
-    {
-        name: "Material UI",
-        src: "/material-ui-svgrepo-com.svg",
-    },
-    {
-        name: "TailwindCSS",
-        src: "/tailwindcss-icon-svgrepo-com.svg",
-    },
-    {
-        name: "Redux",
-        src: "/redux-svgrepo-com.svg",
-    },
-    {
-        name: "HTML5",
-        src: "/html-5-svgrepo-com.svg",
-    },
-    {
-        name: "CSS3",
-        src: "/css-3-svgrepo-com.svg",
-    },
-    {
-        name: "Typescript",
-        src: "/typescript-svgrepo-com.svg",
-    },
-];
-
-const STACKS_BE = [
-    {
-        name: "PHP",
-        src: "/php-svgrepo-com.svg",
-    },
-    {
-        name: "Laravel",
-        src: "/laravel-svgrepo-com.svg",
-    },
-    {
-        name: "NodeJS",
-        src: "/nodejs-svgrepo-com.svg",
-    },
-    {
-        name: "NestJS",
-        src: "/nestjs-svgrepo-com.svg",
-    },
-    {
-        name: "Docker",
-        src: "/docker-svgrepo-com.svg",
-    },
-    {
-        name: "Ubuntu",
-        src: "/ubuntu-svgrepo-com.svg",
-    },
-    {
-        name: "Nginx",
-        src: "/nginx-svgrepo-com.svg",
-    },
-    {
-        name: "PostgreSQL",
-        src: "/postgresql-svgrepo-com.svg",
-    },
-];
+// Tách component StackIcons để tái sử dụng và giữ logic hiển thị danh sách công nghệ
+const StackIcons = ({ list }) => (
+    <div className="grid grid-cols-2 md:grid-cols-4 gap-1 md:gap-8">
+        {list.map((item, idx) => (
+            <div
+                key={idx}
+                className="flex flex-col gap-2 items-center justify-center"
+            >
+                <img src={item.src || "/default-icon.png"} className="w-24" />
+                <p className="text-2xl">{item.name || "Unnamed"}</p>
+            </div>
+        ))}
+    </div>
+);
 
 export default function MyStackContainer() {
-    const renderStackIcons = (list) => {
-        return (
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-1 md:gap-8">
-                {list.map((item, idx) => {
-                    return (
-                        <div
-                            key={idx}
-                            className="flex flex-col gap-2 items-center justify-center"
-                        >
-                            <img src={item.src} className="w-24" />
-                            <p className="text-2xl">{item.name}</p>
-                        </div>
-                    );
-                })}
-            </div>
-        );
+    const handleBackSlide = () => {
+        console.log("Navigate Back");
+    };
+
+    const handleNextSlide = () => {
+        console.log("Navigate Next");
     };
 
     return (
-        <div className="flex items-start justify-center md:mx-48 h-full">
-            <div className="flex flex-col gap-8 ">
-                <GlowingText
-                    text="My Stack"
-                    gradientColors="from-[#857eff] via-[#a69bff] to-[#c3baff]"
-                />
+        <div className="flex items-center md:items-start justify-center md:mx-48 h-full">
+            <div className="relative md:flex flex-col gap-8">
+                {/* Phần tiêu đề và nút điều hướng */}
+                <div className="flex justify-center items-center gap-8">
+                    <Button
+                        onClick={() => {
+                            handleBackSlide();
+                        }}
+                        className="!rounded-full w-16 h-16 !bg-[#6c63ff] !bg-opacity-30 
+                                      hover:!bg-opacity-100 hover:shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#6c63ff,0_0_15px_#6c63ff,0_0_30px_#6c63ff]"
+                    >
+                        <span className="icon-[mdi--navigate-before] w-16 h-16 text-white " />
+                    </Button>
+                    <GlowingText
+                        text="My Stack"
+                        gradientColors="from-[#857eff] via-[#a69bff] to-[#c3baff] my-8 md:my-0"
+                    />
+                    <Button
+                        onClick={() => {
+                            handleNextSlide();
+                        }}
+                        className="!rounded-full w-16 h-16 !bg-[#6c63ff] !bg-opacity-30 
+                                      hover:!bg-opacity-100 hover:shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#6c63ff,0_0_15px_#6c63ff,0_0_30px_#6c63ff]"
+                    >
+                        <span className="icon-[mdi--navigate-next] w-16 h-16 text-white " />
+                    </Button>
+                </div>
 
-                <div className="flex flex-col gap-8">
-                    <div className="flex flex-col md:flex-row gap-16">
-                        <div className="hidden md:flex flex-col items-center justify-center ml:none md:ml-96 gap-4 ">
-                            <h2 className="text-4xl dark:text-white text-blue-600 ">
-                                FE
-                            </h2>
-                            <img
-                                src="./web-page-browser-analysis-screen-svgrepo-com.svg"
-                                alt="frontend_img"
-                                className="w-24 h-24 hidden md:block"
-                            />
-                        </div>
-                        <div
-                            className="border-[#6c63ff] border-opacity-50 border-1 
+                {/* Phần hiển thị Frontend */}
+                <div className="flex flex-col md:flex-row gap-16">
+                    <div className="hidden md:flex flex-col items-center justify-center ml:none md:ml-96 gap-4">
+                        <h2 className="text-4xl dark:text-white text-blue-600">
+                            FE
+                        </h2>
+                        <img
+                            src="./web-page-browser-analysis-screen-svgrepo-com.svg"
+                            alt="frontend_img"
+                            className="w-24 h-24 hidden md:block"
+                        />
+                    </div>
+                    <div
+                        className="border-[#6c63ff] border-opacity-50 border-1 
                                       min-w-96 min-h-64 border-solid 
                                       rouned-none md:rounded-lg p-8 
                                       backdrop-blur-md
-                                      shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#6c63ff,0_0_15px_#6c63ff,0_0_30px_#6c63ff]
-                                      "
-                        >
-                            <div className="text dark:text-white">
-                                {renderStackIcons(STACKS_FE)}
-                            </div>
+                                      shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#6c63ff,0_0_15px_#6c63ff,0_0_30px_#6c63ff]"
+                    >
+                        <div className="text dark:text-white">
+                            <StackIcons list={STACKS_FE} />
                         </div>
                     </div>
-                    <div className="hidden md:flex flex-col-reverse md:flex-row gap-16">
-                        <div
-                            className="border-[#6c63ff] border-opacity-50 border-1 
+                </div>
+
+                {/* Phần hiển thị Backend */}
+                <div
+                    className="absolute bottom-0 md:relative transform transition-transform duration-1000 translate-x-full md:translate-x-0 ml-32 md:ml-0
+                                  flex flex-col-reverse md:flex-row gap-16"
+                >
+                    <div
+                        className="border-[#6c63ff] border-opacity-50 border-1 
                                       min-w-96 min-h-64 border-solid 
-                                      rouned-none md:rounded-lg p-8 shadow-2xl
+                                      rouned-none md:rounded-lg p-8
                                       backdrop-blur-md
-                                      shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#6c63ff,0_0_15px_#6c63ff,0_0_30px_#6c63ff]
-                                      "
-                        >
-                            <div className="text dark:text-white">
-                                {renderStackIcons(STACKS_BE)}
-                            </div>
+                                      shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#6c63ff,0_0_15px_#6c63ff,0_0_30px_#6c63ff]"
+                    >
+                        <div className="text dark:text-white">
+                            <StackIcons list={STACKS_BE} />
                         </div>
-                        <div className="hidden md:flex flex-row justify-center items-center mr:0 md:mr-48 gap-4">
-                            <h2 className="text-4xl dark:text-white text-blue-600 ">
-                                BE
-                            </h2>
-                            <img
-                                src="/server-network-part-2-svgrepo-com.svg"
-                                alt="backend_img"
-                                className="w-24 h-24 hidden md:block"
-                            />
-                        </div>
+                    </div>
+                    <div className="hidden md:flex flex-row justify-center items-center mr:0 md:mr-48 gap-4">
+                        <h2 className="text-4xl dark:text-white text-blue-600">
+                            BE
+                        </h2>
+                        <img
+                            src="/server-network-part-2-svgrepo-com.svg"
+                            alt="backend_img"
+                            className="w-24 h-24 hidden md:block"
+                        />
                     </div>
                 </div>
             </div>
